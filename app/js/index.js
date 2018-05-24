99973d82f1306324efe2283d4bfe90fd543de1f1
Guillaume <guillaume@rimbault.org>
2018-05-24 17:29:19 +0200

    keyS

--- app/js/index.js
+++ app/js/index.js
@@ -3,43 +3,80 @@
     const { ipcRenderer } = require('electron');
     const { BrowserWindow } = require('electron').remote;
 
-    var button = document.getElementById("button1");
+    var button61;
+    var button62;
+
+    var buttonList;
+
+    var mouseDown = false;
 
     function init() {
-        console.log('init')
+        window.buttonList = {};
+        window.buttonList.button70 = document.getElementById('button-70');
+        window.buttonList.button71 = document.getElementById('button-71');
 
-        window.onkeydown = function (e) {
-            var key = e.keyCode ? e.keyCode : e.which;
-            if (key == 71)
-                button.dispatchEvent(new Event("mousedown"));
-        }
+        buttonList = document.getElementsByClassName('btn-clickable');
 
-        window.onkeyup = function (e) {
-            var key = e.keyCode ? e.keyCode : e.which;
-            if (key == 71) 
-                button.dispatchEvent(new Event("mouseup"));
-        }
-        
         init_event();
     };
 
     function init_event() {
-        // Minimize task
+
+        // window function
         document.getElementById('min-btn').addEventListener('click', function () {
             let window = BrowserWindow.getFocusedWindow();
             window.minimize();
         });
-
-        // Maximize window
         document.getElementById('max-btn').addEventListener('click', function () {
             ipcRenderer.send('change-size');
         });
-
-        // Close app
         document.getElementById('exit-btn').addEventListener('click', function () {
             let window = BrowserWindow.getFocusedWindow();
             window.close();
         });
+
+
+        window.onkeydown = function (e) {
+            var key = e.keyCode ? e.keyCode : e.which;
+            console.log(key);
+            if (window.buttonList['button' + key] != undefined)
+                window.buttonList['button' + key].dispatchEvent(new Event("mousedown"));
+        }
+
+        window.onkeyup = function (e) {
+            var key = e.keyCode ? e.keyCode : e.which;
+
+            if (window.buttonList['button' + key] != undefined)
+                window.buttonList['button' + key].dispatchEvent(new Event("mouseup"));
+        }
+
+        
+        document.body.onmousedown = function () {
+            mouseDown = true;
+        }
+        document.body.onmouseup = function () {
+            mouseDown = false;
+        }
+
+        for (var i = 0; i < buttonList.length; i++) {
+
+            buttonList[i].addEventListener('mousedown', function (e) {
+                e.target.classList.add('clicked');
+            })
+
+            buttonList[i].addEventListener('mouseup', function (e) {
+                e.target.classList.remove('clicked');
+            })
+
+            buttonList[i].addEventListener('pointerout', function (e) {
+                e.target.classList.remove('clicked');
+            })
+
+            buttonList[i].addEventListener('pointerenter', function (e) {
+                if (mouseDown)
+                    e.target.classList.add('clicked');
+            })
+        }
     }
 
     document.onreadystatechange = () => {
